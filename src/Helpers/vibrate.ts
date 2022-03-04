b475ca1b5a1f0f25b6a6d62b2143053975cda45e
Stephen (Stevo) Saucier <stephen.saucier@clearspend.com>
2022-03-04 15:16:01 +0000

    Merged in MA-286 (pull request #77)
    [MA-286] Simplified vibrate logic to avoid Android crash
    
    * [MA-286] Simplified vibrate logic to avoid Android crash
    
    
    Approved-by: Rory O'Connor

--- src/Helpers/vibrate.ts
+++ src/Helpers/vibrate.ts
@@ -1,10 +1,5 @@
-import { Platform, Vibration } from 'react-native';
+import { Vibration } from 'react-native';
 
 export const vibrate = () => {
-  Vibration.vibrate(
-    Platform.select({
-      android: [0, 80, 30, 150],
-      ios: [0, 30],
-    }),
-  );
+  Vibration.vibrate();
 };
