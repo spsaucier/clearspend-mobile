3b5e61ef310d43d0c174e12c583cb7b831573a9d
Nick Clark <nick.clark@dovetailstudios.com>
2022-04-06 02:33:30 +0000

    [ma-331] fix-profile-font (pull request #143)

--- src/Containers/Profile/ProfileScreen.tsx
+++ src/Containers/Profile/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, ScrollView, PixelRatio } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/core';
@@ -22,81 +22,84 @@ const ProfileScreen = () => {
   const { logout } = useAuthentication();
   const version = getVersion();
   const buildNumber = getBuildNumber();
+
   return (
     <SafeAreaView style={tw`flex-1 bg-secondary`} edges={['top', 'bottom']}>
       <FocusAwareStatusBar backgroundColor={tw.color('secondary')} barStyle="light-content" />
       <View style={tw`p-5`}>
         <BackButtonNavigator theme="dark" />
       </View>
-      <View style={[tw`h-1/3 flex-col justify-center bg-secondary px-5 pt-12 pb-16`]}>
-        {isLoading || error || !user ? (
-          <ActivityIndicator />
-        ) : (
-          <>
-            <CSText style={tw`text-3xl font-montreal font-light text-white mb-5`}>
-              {`${user.firstName} ${user.lastName}`}
-            </CSText>
-            <View style={tw`flex-row mb-3`}>
-              <EmailIcon size={20} />
-              <CSText style={tw`text-white text-sm ml-3 leading-5`}>{user.email}</CSText>
+      <ScrollView>
+        <View style={[tw`bg-secondary px-5`]}>
+          {isLoading || error || !user ? (
+            <View style={tw`items-center`}>
+              <ActivityIndicator />
             </View>
-            {user.phone ? (
+          ) : (
+            <>
+              <CSText style={tw`text-3xl font-montreal font-light text-white mb-10`}>
+                {`${user.firstName} ${user.lastName}`}
+              </CSText>
               <View style={tw`flex-row mb-3`}>
-                <PhoneIcon size={20} />
-                <CSText style={tw`text-white text-sm ml-3 leading-5`}>
-                  {formatPhone(user.phone)}
-                </CSText>
+                <EmailIcon size={20 * PixelRatio.getFontScale()} />
+                <CSText style={tw`text-white text-sm ml-3 leading-5 flex-shrink-1`}>{user.email}</CSText>
               </View>
-            ) : null}
-            <AddressDisplay address={user.address || {}} />
-          </>
-        )}
-      </View>
-      <View style={tw`px-6 flex-1 justify-between`}>
-        {/* Bottom white area */}
-        <View style={tw`bg-white rounded-1`}>
-          <ProfileMenuRow
-            title={t('profile.profileMenu.updatePersonalDetails')}
-            onPress={() => {
-              navigate(MainScreens.UpdateAccount);
-            }}
-            style={tw`h-14 px-4`}
-            showBottomBorder
-          />
-          <ProfileMenuRow
-            title={t('profile.profileMenu.loginOptions')}
-            onPress={() => {
-              navigate(MainScreens.LoginOptions);
-            }}
-            style={tw`h-14 px-4`}
-            showBottomBorder
-          />
-          <ProfileMenuRow
-            title={t('profile.profileMenu.activateCard')}
-            onPress={() => {
-              navigate(MainScreens.ActivateCard);
-            }}
-            style={tw`h-14 px-4`}
-            showBottomBorder
-          />
-          <ProfileMenuRow
-            title={t('profile.legalDocs.title')}
-            onPress={() => {
-              navigate(MainScreens.LegalDocuments);
-            }}
-            style={tw`h-14 px-4`}
-          />
+              {user.phone ? (
+                <View style={tw`flex-row mb-3`}>
+                  <PhoneIcon size={20 * PixelRatio.getFontScale()} />
+                  <CSText style={tw`text-white text-sm ml-3 leading-5`}>
+                    {formatPhone(user.phone)}
+                  </CSText>
+                </View>
+              ) : null}
+              <AddressDisplay address={user.address || {}} />
+            </>
+          )}
         </View>
-
-        <TouchableOpacity onPress={logout} style={tw`flex-row justify-center mb-10`}>
+        <View style={tw`px-5 mt-14 mb-32`}>
+          {/* Bottom white area */}
+          <View style={tw`bg-white rounded-1`}>
+            <ProfileMenuRow
+              title={t('profile.profileMenu.updatePersonalDetails')}
+              onPress={() => {
+                navigate(MainScreens.UpdateAccount);
+              }}
+              style={tw`h-14 px-4`}
+              showBottomBorder
+            />
+            <ProfileMenuRow
+              title={t('profile.profileMenu.loginOptions')}
+              onPress={() => {
+                navigate(MainScreens.LoginOptions);
+              }}
+              style={tw`h-14 px-4`}
+              showBottomBorder
+            />
+            <ProfileMenuRow
+              title={t('profile.profileMenu.activateCard')}
+              onPress={() => {
+                navigate(MainScreens.ActivateCard);
+              }}
+              style={tw`h-14 px-4`}
+              showBottomBorder
+            />
+            <ProfileMenuRow
+              title={t('profile.legalDocs.title')}
+              onPress={() => {
+                navigate(MainScreens.LegalDocuments);
+              }}
+              style={tw`h-14 px-4`}
+            />
+          </View>
+        </View>
+      </ScrollView>
+      <View style={tw`items-center mt-auto px-5 py-4`}>
+        <TouchableOpacity onPress={logout}>
           <CSText style={tw`text-primary py-3`}>{t('profile.profileMenu.logOut')}</CSText>
         </TouchableOpacity>
-
-        <View style={tw`items-center mb-2`}>
-          <CSText style={tw`text-sm text-white pb-2`}>
-            {t('profile.appVersion', { appVersion: `${version} (${buildNumber})` })}
-          </CSText>
-        </View>
+        <CSText style={tw`text-sm text-white`} allowFontScaling={false}>
+          {t('profile.appVersion', { appVersion: `${version} (${buildNumber})` })}
+        </CSText>
       </View>
     </SafeAreaView>
   );
