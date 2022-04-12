721ff0b7b429508ec39762c688aa9634d570eaa4
Rory O'Connor <rory.oconnor@dovetailstudios.com>
2022-04-12 22:24:22 +0000

    [MA-407] Disable terms interstitial checks due to backend issue (pull request #161)
    Approved-by: Rodrigo Mathias

--- src/Hooks/useRequireUserAction.ts
+++ src/Hooks/useRequireUserAction.ts
@@ -1,15 +1,15 @@
 import { useEffect } from 'react';
 import { ParamListBase, useNavigation, useRoute } from '@react-navigation/core';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { useMMKVNumber } from 'react-native-mmkv';
-import { differenceInHours, subHours } from 'date-fns';
+// import { useMMKVNumber } from 'react-native-mmkv';
+// import { differenceInHours, subHours } from 'date-fns';
 
 import { MainScreens } from '@/Navigators/NavigatorTypes';
 import useRequireBioOrPasscodeSetup from './useRequireBioOrPasscodeSetup';
 import useRequire2FA from './useRequire2FA';
-import { useTermsAndConditionsTimestampDetails } from '@/Queries/termsAndConditions';
+// import { useTermsAndConditionsTimestampDetails } from '@/Queries/termsAndConditions';
 
-const TERMS_AND_CONDITIONS_LAST_CHECK = 'TERMS_AND_CONDITIONS_LAST_CHECK';
+// const TERMS_AND_CONDITIONS_LAST_CHECK = 'TERMS_AND_CONDITIONS_LAST_CHECK';
 
 const useRequireUserAction = (): void => {
   const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
@@ -20,20 +20,21 @@ const useRequireUserAction = (): void => {
   const { loading: loadingBioPasscode, shouldAct: needsBioPasscode } =
     useRequireBioOrPasscodeSetup();
 
-  const { refetch: checkTermsAndConditions } = useTermsAndConditionsTimestampDetails();
-  const [termsAndConditionsLastCheckDateTime] = useMMKVNumber(TERMS_AND_CONDITIONS_LAST_CHECK);
+  // const { refetch: checkTermsAndConditions } = useTermsAndConditionsTimestampDetails();
+  // const [termsAndConditionsLastCheckDateTime] = useMMKVNumber(TERMS_AND_CONDITIONS_LAST_CHECK);
 
-  useEffect(() => {
-    const lastCheckDt = termsAndConditionsLastCheckDateTime || subHours(new Date(), 2);
-    if (differenceInHours(new Date(), lastCheckDt) >= 1) {
-      checkTermsAndConditions().then((response) => {
-        const { isAcceptedTermsAndConditions } = response.data!;
-        if (!isAcceptedTermsAndConditions) {
-          navigation.replace(MainScreens.UpdatedTermsAndConditionsScreen);
-        }
-      });
-    }
-  }, [checkTermsAndConditions, termsAndConditionsLastCheckDateTime, navigation]);
+  // TODO: Re enable terms and conditions check when backend is fixed
+  // useEffect(() => {
+  //   const lastCheckDt = termsAndConditionsLastCheckDateTime || subHours(new Date(), 2);
+  //   if (differenceInHours(new Date(), lastCheckDt) >= 1) {
+  //     checkTermsAndConditions().then((response) => {
+  //       const { isAcceptedTermsAndConditions } = response.data!;
+  //       if (!isAcceptedTermsAndConditions) {
+  //         navigation.replace(MainScreens.UpdatedTermsAndConditionsScreen);
+  //       }
+  //     });
+  //   }
+  // }, [checkTermsAndConditions, termsAndConditionsLastCheckDateTime, navigation]);
 
   useEffect(() => {
     if (
