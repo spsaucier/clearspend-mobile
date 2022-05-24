b9549d42d81568ea57bcd5a4acdd34d679068d98
Rory O'Connor <rory.oconnor@dovetailstudios.com>
2022-05-24 04:40:12 +0000

    [MA-270] Upload Android DEV builds to SauceLabs (pull request #234)
    Approved-by: Nick Clark

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -10,16 +10,27 @@ if [[ "$APPCENTER_XCODE_PROJECT" ]]; then
 else
   echo "Android only post build script"
 
-  # Upload successful builds to firebase
+  # Upload successful builds to Firebase and SauceLabs
   if [[ "$AGENT_JOBSTATUS" == "Succeeded" ]]; then
-
     lowercase_env_name=$(tr '[:upper:]' '[:lower:]' <<<"${CS_ENV_NAME}")
+    aab_name="app-$lowercase_env_name-release.aab"
+    aab_location="android/app/build/outputs/bundle/$APPCENTER_ANDROID_VARIANT/$aab_name"
 
     npm install -g firebase-tools
-    firebase appdistribution:distribute "android/app/build/outputs/bundle/$APPCENTER_ANDROID_VARIANT/app-$lowercase_env_name-release.aab" \
+    echo "Uploading build to Firebase"
+    firebase appdistribution:distribute "$aab_location" \
       --app "$FIREBASE_APP_ID" \
       --groups "clearspend-testers"
     # TODO provide release notes
     # --release-notes ""
+
+    # Only upload DEV builds
+    if [[ "$CS_ENV_NAME" == "DEV" ]]; then
+      echo "Uploading build to SauceLabs"
+      curl \
+        -F "payload=@$aab_location" \
+        -F name="$aab_name" \
+        -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" 'https://api.us-west-1.saucelabs.com/v1/storage/upload'
+    fi
   fi
 fi
